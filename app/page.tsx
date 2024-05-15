4c3bddfb25830fef64eb11b3116dd41e6cd9d904
facundojsole <facundojsole@gmail.com>
2024-05-15 17:00:04 +0000

    fix responsive

--- app/page.tsx
+++ app/page.tsx
@@ -40,11 +40,11 @@ let rotate4 = useTransform(scrollYProgress, [0.8, 1], [5, 0]);
         <h1>Home</h1>
         <p>This is the home page</p>
       </Section>
-      <Section scale={scale2} rotate={rotate2} classes="sticky top-0 h-[150dvh] lg:h-screen p-24 text-black bg-texture bg-repeat">
+      <Section scale={scale2} rotate={rotate2} classes="sticky top-0 grid lg:gap-12 place-items-center h-screen p-8 lg:p-24 text-black bg-texture bg-repeat">
         <a href="https://www.humankind.art/wrestle" target="_blank" rel="noopener noreferrer" className="flex justify-center"><img src="/assets/wrestle-icon.svg" /></a>
-        <article className="grid grid-cols-1 lg:grid-cols-[minmax(300px,600px),1fr] gap-10 place-items-center pt-12">
+        <article className="grid grid-cols-1 lg:grid-cols-[minmax(300px,600px),1fr] gap-10 lg:place-items-center">
           <img src="/assets/wrestle-avatars.gif" alt="Avatars of the NFTs called Humankind" className="max-w-[200px] mx-auto lg:max-w-[500px] rounded-xl"/>
-          <p className="max-w-[50ch] text-sm lg:text-2xl p-4 lg:p-12 bg-white border-2 border-black rounded-xl">Step into the arena with Wrestle, the latest game feature in Herokind. Pit your Humankind characters against a diverse array of monsters, from the ferocious Alligatornado to the cunning Nerdy Dragon. Strategize your moves and watch your characters grow in this turn-based adventure.</p>
+          <p className="max-w-[50ch] text-sm lg:text-2xl mx-4 p-4 lg:p-12 bg-white border-2 border-black rounded-xl">Step into the arena with Wrestle, the latest game feature in Herokind. Pit your Humankind characters against a diverse array of monsters, from the ferocious Alligatornado to the cunning Nerdy Dragon. Strategize your moves and watch your characters grow in this turn-based adventure.</p>
         </article>
       </Section>
       <Section scale={scale3} rotate={rotate3} classes="sticky top-0 h-screen bg-yellow-400 text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]">
