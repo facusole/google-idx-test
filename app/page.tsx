9ea3bd015454bc15747619de085eec4eb61e888f
facundojsole <facundojsole@gmail.com>
2024-05-15 16:50:42 +0000

    add wrestle section with responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -25,12 +25,12 @@ requestAnimationFrame(raf)
 }, [])
 
 // Sections transform values
-let scale = useTransform(scrollYProgress, [0, 0.25], [1, 0.8]);
-let rotate = useTransform(scrollYProgress, [0, 0.25], [0, -5]);
-let scale2 = useTransform(scrollYProgress, [0.2, 0.35], [0.8, 1]);
-let rotate2 = useTransform(scrollYProgress, [0.2, 0.35], [5, 0]);
-let scale3 = useTransform(scrollYProgress, [0.5, 0.65], [0.8, 1]);
-let rotate3 = useTransform(scrollYProgress, [0.5, 0.65], [-5, 0]);
+let scale = useTransform(scrollYProgress, [0, 0.2], [1, 0.8]);
+let rotate = useTransform(scrollYProgress, [0, 0.2], [0, -5]);
+let scale2 = useTransform(scrollYProgress, [0.2, 0.3], [0.8, 1]);
+let rotate2 = useTransform(scrollYProgress, [0.2, 0.3], [5, 0]);
+let scale3 = useTransform(scrollYProgress, [0.5, 0.7], [0.8, 1]);
+let rotate3 = useTransform(scrollYProgress, [0.5, 0.7], [-5, 0]);
 let scale4 = useTransform(scrollYProgress, [0.8, 1], [0.8, 1]);
 let rotate4 = useTransform(scrollYProgress, [0.8, 1], [5, 0]);
 
@@ -40,9 +40,12 @@ let rotate4 = useTransform(scrollYProgress, [0.8, 1], [5, 0]);
         <h1>Home</h1>
         <p>This is the home page</p>
       </Section>
-      <Section scale={scale2} rotate={rotate2} classes="sticky top-0 h-screen bg-blue-400 text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]">
-        <h1>About me</h1>
-        <p>This is the about me section</p>
+      <Section scale={scale2} rotate={rotate2} classes="sticky top-0 h-screen p-24 text-black bg-texture bg-repeat">
+        <a href="https://www.humankind.art/wrestle" target="_blank" rel="noopener noreferrer" className="flex justify-center"><img src="/assets/wrestle-icon.svg" /></a>
+        <article className="grid grid-cols-1 lg:grid-cols-[minmax(300px,600px),1fr] gap-10 place-items-center pt-12">
+          <img src="/assets/wrestle-avatars.gif" alt="Avatars of the NFTs called Humankind" className="max-w-[200px] mx-auto lg:max-w-[500px] rounded-xl"/>
+          <p className="max-w-[50ch] text-md lg:text-2xl p-4 lg:p-12 bg-white border-2 border-black rounded-xl">Step into the arena with Wrestle, the latest game feature in Herokind. Pit your Humankind characters against a diverse array of monsters, from the ferocious Alligatornado to the cunning Nerdy Dragon. Strategize your moves and watch your characters grow in this turn-based adventure.</p>
+        </article>
       </Section>
       <Section scale={scale3} rotate={rotate3} classes="sticky top-0 h-screen bg-yellow-400 text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]">
         <h1>About me</h1>
